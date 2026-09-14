8277b11ca3ca6f5107276ed808a24118bfa92c68
AprajitaMishra-CS <mishraaprajita700@gmail.com>
2026-09-14 15:11:25 +0530

    Initial commit - Created Pages and Validation

---
+++ models/listing.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review");
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: String,
+    image: {
+        filename: {
+            type: String,
+            default: "listingimage"
+        },
+        url: {
+            type: String,
+            default: "https://unsplash.com/photos/swimming-pool-with-lounge-chairs-xvnPgm6lnAs"
+        }
+    },
+    price: Number,
+    location: String,
+    country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if(listing){
+        await Review.deleteMany({_id: {$in: listing.reviews}});
+    } 
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;

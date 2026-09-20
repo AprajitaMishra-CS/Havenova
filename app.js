72dae143eea50308a50358d0576c4081d82a0949
AprajitaMishra-CS <mishraaprajita700@gmail.com>
2026-09-21 01:17:32 +0530

    Fix Root Route

--- app.js
+++ app.js
@@ -72,9 +72,9 @@ const sessionOptions = {
     }
 };
 
-// app.get("/", (req, res) => {
-//     res.send("Hi, I am root");
-// });
+app.get("/", (req, res) => {
+    res.render("/listings");
+});
 
 app.use(session(sessionOptions));
 app.use(flash());
